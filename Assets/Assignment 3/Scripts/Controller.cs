b7820ede14dec7bb41cb8b586ee828c779049963
Knappkin <44062671+Knappkin@users.noreply.github.com>
2025-03-28 23:45:25 -0400

    Started working on the scripts for the controller and interactable objects
    Controller:
    Character is set to active in start
    
    left click calls an event which is subscribed to by each interactable object (current event is a test)
    
    In update - character position is set to mouseposition
    
    Interactable object:
    Is added as a listener to the controller test event in start
    
    if object pressed, colour changes
    
    when invoked, event calls speak function (is a test function)

--- Assets/Assignment 3/Scripts/Controller.cs
+++ Assets/Assignment 3/Scripts/Controller.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
 
 public class Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    // Character UI - follows the mouse when object not possessed
+    public GameObject character;
+
+    public UnityEvent sayHi;
     void Start()
     {
-        
+        character.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+        if (Input.GetMouseButtonDown(0))
+        {
+            //character.SetActive(false);
+            sayHi.Invoke();
+        }
+
+        if (character != null)
+        {
+            character.transform.position = Input.mousePosition;
+        }
     }
 }
--- Assets/Assignment 3/Scripts/InteractableObject.cs
+++ Assets/Assignment 3/Scripts/InteractableObject.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InteractableObject : MonoBehaviour
 {
+
+    public string message;
+
+    public Controller controller;
+
+    public UnityEvent<GameObject> OnClick;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller.sayHi.AddListener(Speak);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0) && GetComponent<SpriteRenderer>().bounds.Contains(mousePos))
+        {
+            GetComponent<SpriteRenderer>().color = Color.yellow;
+        }
+        {
+
+        }
+    }
+
+    public void Speak()
+    {
+        Debug.Log(message);
     }
 }
