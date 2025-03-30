2e8f36eab3a506d4a3e0b03d0ce7267527cec911
Knappkin <44062671+Knappkin@users.noreply.github.com>
2025-03-29 20:33:07 -0400

    Started phone script
    Phone jumps using coroutine - currently starts right away and goes infinitely
    
    Next step: figure out how to connect the individual phone, paper and key scripts to their respective interactable object. Also decide how im going to make it only go a set amount of times

--- Assets/Assignment 3/Scripts/Phone.cs
+++ Assets/Assignment 3/Scripts/Phone.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Phone : MonoBehaviour
 {
+
+    public AnimationCurve jumpCurve;
+    Vector2 startingPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingPos = transform.position;
+        StartCoroutine(RingThePhone());
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private IEnumerator RingThePhone()
+    {
+        Vector2 pos = transform.position;
+        float t = 0;
+        while (true)
+        {
+
+            t += Time.deltaTime;
+
+            if (t > 1)
+            {
+                t = 0;
+            }
+
+            pos.y = startingPos.y + jumpCurve.Evaluate(t*1.5f);
+            transform.position = pos;
+            yield return null;
+        }
         
     }
 }
